262e3201098cb0f7e1c89bf28ced35e72293a503
Tanya Kalkuta <Tanyakalkuta@gmail.com>
2021-04-19 23:27:12 +0300

    поиск стран

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import './styles.css';
+
+import { debounce } from 'lodash';
+import countriesList from './templates/countries-list.hbs'
+import countryCard from './templates/country-card.hbs'
+import refs from './js/refs'
+import fetchCountries from './js/fetchCountries'
+import makeCountryCard from './js/makeCountryMarkupList'
+import makeError from './js/makeError'
+
+refs.input.addEventListener('input', debounce(fetchCountriesList, 1000))
+
+function fetchCountriesList(e) {
+    const name = e.target.value
+
+    if (e.target.value === '') {
+      makeCountryCard('');
+    }
+    else {
+      fetchCountries(name)
+        .then(countries => {
+          if (countries.length === 1) {
+            makeCountryCard(countryCard(countries[0]));
+          }
+          else if (countries.length > 10) {
+            makeError();
+          }
+          else {
+            makeCountryCard(countriesList(countries));
+          }
+        })
+        .catch(error => makeCountryCard('<h1>Sorry! Could not find this page</h1>'));
+      }
+    }
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,11 @@
+export default (searchQuery) => {
+    
+    return  fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}`)
+        .then(response => {
+            if (!response.ok) {
+                throw error;
+            }
+            return response.json();
+            })
+}
+    
